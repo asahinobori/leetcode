7cd6dadaef6fb6e1f68ea262d3aae266f754ee4c
asahinobori <xusheng41@gmail.com>
2016-02-23 15:06:11 +0800

    add cpp version and make c version compact

--- Algorithms/27_Remove_Element/rmElement.c
+++ Algorithms/27_Remove_Element/rmElement.c
@@ -2,8 +2,7 @@ int removeElement(int* nums, int numsSize, int val) {
     int newLength = 0;
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] != val) {
-            newLength++;
-            nums[newLength - 1] = nums[i];
+            nums[newLength++] = nums[i];
         }
     }
     return newLength;

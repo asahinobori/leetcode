823a87fa82647448d21cd7fac30837f92653b5c9
asahinobori <xusheng41@gmail.com>
2016-03-07 17:28:02 +0800

    recursive and iterative version, smart code and cpp version

--- Algorithms/82_Remove_Duplicates/deleteDuplicates.c
+++ Algorithms/82_Remove_Duplicates/deleteDuplicates.c
@@ -5,48 +5,44 @@
  *     struct ListNode *next;
  * };
  */
+
+// 递归版
 struct ListNode* deleteDuplicates(struct ListNode* head) {
-    if (head == NULL) return head;
-    // 题目已说这单链表已经排序
-    // 这里(偷懒)假设是递增排序，实际要简单测试是递增还是递减
-    // 取一个val不会与单链表中各节点val重复的值作为mark值
-    // 鉴于上面的假设，取mark值为比头节点还要小的值，如head->val - 1
-    int mark = head->val - 1;
-    // top指向当前判断中(是否存在重复字符)的子字符串的头
-    struct ListNode* top = head;
-    // cursor一直向前移动
-    struct ListNode* cursor = head->next;
-    // 是否存在重复字符串
-    bool flag = false;
-    while (cursor) {
-        if (cursor->val == top->val) {
-            cursor = cursor->next;
-            top->next = cursor;
-            flag = true;
+    if (!head || !head->next) return head;
+
+    struct ListNode *p = head->next;
+    if (head->val == p->val) {
+        while (p && head->val == p->val) {
+            p = p->next;
+        }
+        return deleteDuplicates(p);
+    } else {
+        head->next = deleteDuplicates(head->next);
+        return head;
+    }
+}
+
+// 迭代版
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (!head) return head;
+    struct ListNode dummy;
+    dummy.val = INT_MIN;
+    dummy.next = head;
+    struct ListNode *prev = &dummy, *cur = head;
+    while (cur != NULL) {
+        bool duplicated = false;
+        while (cur->next != NULL && cur->val == cur->next->val) {
+            duplicated = true;
+            cur = cur->next;
+        }
+        if (duplicated) {
+            cur = cur->next;
             continue;
         }
-        if (flag) {
-            top->val = mark;
-            flag = false;
-        }
-        top = cursor;
-        cursor = cursor->next;
+        prev->next = cur;
+        prev = prev->next;
+        cur = cur->next;
     }
-    if (flag) {
-        top->val = mark;
-        flag = false;
-    }
-    top = head;
-    while (top && top->val == mark) {
-        head = top->next;
-        top = top->next;
-    }
-    while (top && top->next) {
-        if (top->next->val == mark) {
-            top->next = top->next->next;
-            continue;
-        }
-        top = top->next;
-    }
-    return head;
+    prev->next = cur;
+    return dummy.next;
 }
---
+++ Algorithms/82_Remove_Duplicates/deleteDuplicates.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// 递归版
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode *p = head->next;
+        if (head->val == p->val) {
+            while (p && head->val == p->val) {
+                ListNode *tmp = p;
+                p = p->next;
+                delete tmp;
+            }
+            delete head;
+            return deleteDuplicates(p);
+        } else {
+            head->next = deleteDuplicates(head->next);
+            return head;
+        }
+    }
+};
+
+// 迭代版
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (head == nullptr) return head;
+        ListNode dummy(INT_MIN);
+        dummy.next = head;
+        ListNode *prev = &dummy, *cur = head;
+        while (cur != nullptr) {
+            bool duplicated = false;
+            while (cur->next != nullptr && cur->val == cur->next->val) {
+                duplicated = true;
+                ListNode *temp = cur;
+                cur = cur->next;
+                delete temp;
+            }
+            if (duplicated) {
+                ListNode *temp = cur;
+                cur = cur->next;
+                delete temp;
+                continue;
+            }
+            prev->next = cur;
+            prev = prev->next;
+            cur = cur->next;
+        }
+        prev->next = cur;
+        return dummy.next;
+    }
+};

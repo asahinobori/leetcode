7fea3d34955aaac22d47b3df6d06e6959ee57e6f
asahinobori <xusheng41@gmail.com>
2016-03-09 10:11:02 +0800

    BTree max depth cpp version, what a short code !

--- Algorithms/104_Maximum_Depth_BTree/maxDepth.c
+++ Algorithms/104_Maximum_Depth_BTree/maxDepth.c
@@ -10,5 +10,5 @@ int maxDepth(struct TreeNode* root) {
     if (!root) return 0;
     int leftDepth = maxDepth(root->left);
     int rightDepth = maxDepth(root->right);
-    return (leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1);
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
 }

858d21f9361cda123e7aac58384b7fd0aa599263
asahinobori <xusheng41@gmail.com>
2016-03-13 18:01:17 +0800

    merge two sorted list, compact code

--- Algorithms/21_Merge_Two_Sorted_Lists/mergeTwoLists.c
+++ Algorithms/21_Merge_Two_Sorted_Lists/mergeTwoLists.c
@@ -6,20 +6,18 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode head = {-1, NULL};
-    struct ListNode* cur = &head;
+    struct ListNode dummy = {-1, NULL};
+    struct ListNode* cur = &dummy;
     while (l1 != NULL && l2 != NULL) {
         if (l1->val < l2->val) {
             cur->next = l1;
             l1 = l1->next;
-            cur = cur->next;
         } else {
             cur->next = l2;
             l2 = l2->next;
-            cur = cur->next;
         }
+        cur = cur->next;
     }
-    if (!l1) cur->next = l2;
-    if (!l2) cur->next = l1;
-    return head.next;
+    cur->next = l1 != NULL ? l1 : l2;
+    return dummy.next;
 }

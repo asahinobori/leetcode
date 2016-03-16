e85c53e655231cc3e979d4803bf4ed4df1a2f9a1
asahinobori <xusheng41@gmail.com>
2016-03-16 10:27:07 +0800

    is balance binary tree, cut branch and use only one recursive loop

--- Algorithms/110_Balanced_Binary_Tree/isBalanced.c
+++ Algorithms/110_Balanced_Binary_Tree/isBalanced.c
@@ -11,15 +11,12 @@ int depth(struct TreeNode* root) {
     if (root == NULL) return 0;
     int depLeft = depth(root->left);
     int depRight = depth(root->right);
+    int diff = depLeft - depRight;
+    // 剪枝
+    if (depLeft < 0 || depRight < 0 || diff < -1 || diff > 1) return -1;
     return (depLeft > depRight ? depLeft : depRight) + 1;
 }
 
 bool isBalanced(struct TreeNode* root) {
-    if (root == NULL) return true;
-    int diff = depth(root->left) - depth(root->right);
-    if (diff > 1 || diff < -1) {
-        return false;
-    } else {
-        return isBalanced(root->left) && isBalanced(root->right);
-    }
+    return depth(root) >= 0;
 }
